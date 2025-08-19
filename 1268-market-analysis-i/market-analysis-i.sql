18c6ce24b5a063b75332b7276c121df92eedf849
Jingyu Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2025-08-19 17:31:25 -0500

    Time: 1112 ms (88.55%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1268-market-analysis-i/market-analysis-i.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+SELECT u.user_id AS buyer_id, u.join_date, COUNT(o.order_id) AS orders_in_2019
+FROM Users u
+LEFT JOIN Orders o 
+ON o.buyer_id = u.user_id AND YEAR(o.order_date) = 2019
+GROUP BY u.user_id  
+ORDER BY u.user_id;
+
