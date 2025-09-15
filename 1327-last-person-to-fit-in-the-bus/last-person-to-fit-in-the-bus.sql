80fb043c1618be6c29e9d899bfe1e70c59ff4f74
Jingyu Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2025-09-15 12:37:45 -0500

    Time: 754 ms (79.82%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,11 @@
+WITH summed AS (
+    SELECT person_name, weight, turn, SUM(weight) OVER (ORDER BY turn) AS total_weight
+    FROM Queue
+    ORDER BY turn
+)
+SELECT person_name
+FROM summed
+WHERE total_weight <= 1000
+ORDER BY Turn DESC
+LIMIT 1
+;
