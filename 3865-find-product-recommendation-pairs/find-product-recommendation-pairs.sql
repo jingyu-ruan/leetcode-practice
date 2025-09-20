1ddf4903c34e478909d9e086d1795958cac7b244
Jingyu Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2025-09-20 13:22:27 -0500

    Time: 781 ms (16.16%) | Memory: 0B (100.00%) - LeetSync

--- 3865-find-product-recommendation-pairs/find-product-recommendation-pairs.sql
+++ 3865-find-product-recommendation-pairs/find-product-recommendation-pairs.sql
@@ -1,33 +1,14 @@
-# Write your MySQL query statement below
-/*
-WITH merged AS (
-    SELECT pp.user_id, pp.product_id
-    FROM ProductPurchases pp
-    CROSS JOIN ProductPurchases pp2
-    WHERE pp.user_id = pp2.user_id
-)
-*/
-WITH merged AS (
-    SELECT pp.user_id AS id1,
-    pp.product_id AS product1_id,
-    i.category AS product1_category,
-    pp2.user_id AS id2,
-    pp2.product_id AS product2_id,
-    i2.category AS product2_category, 
-    CONCAT(pp.product_id, pp2.product_id) AS cct, 
-    COUNT(CONCAT(pp.product_id, pp2.product_id)) AS customer_count
-    FROM ProductPurchases pp
-    CROSS JOIN ProductPurchases pp2
-    JOIN ProductInfo i ON i.product_id = pp.product_id
-    JOIN ProductInfo i2 ON i2.product_id = pp2.product_id
-    WHERE pp.user_id = pp2.user_id AND pp.product_id < pp2.product_id
-    GROUP BY CONCAT(pp.product_id, pp2.product_id)
-    HAVING customer_count >= 3
-)
-
-SELECT product1_id, product2_id, product1_category, product2_category, customer_count
-FROM merged
-ORDER BY customer_count DESC, product1_id, product2_id
+SELECT p1.product_id AS product1_id, 
+       p2.product_id AS product2_id,
+       pi1.category AS product1_category, 
+       pi2.category AS product2_category, 
+       COUNT(*) AS customer_count
+FROM ProductPurchases p1
+JOIN ProductPurchases p2 ON p1.user_id = p2.user_id 
+LEFT JOIN ProductInfo pi1 ON p1.product_id = pi1.product_id 
+LEFT JOIN ProductInfo pi2 ON p2.product_id = pi2.product_id
+WHERE p1.product_id < p2.product_id
+GROUP BY product1_id, product2_id 
+HAVING customer_count >= 3
+ORDER BY customer_count DESC, product1_id, product2_id 
 ;
-
-
