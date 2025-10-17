8bf0d3fce27ac99d4123b3288a2435c6a75e1b0c
Jingyu Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2025-10-17 10:35:06 -0500

    Time: 287 ms (70.80%) | Memory: 0B (100.00%) - LeetSync

--- 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -1,16 +1,11 @@
-# Write your MySQL query statement below
-WITH merged AS(
-SELECT requester_id AS id
-FROM RequestAccepted
-UNION ALL
-SELECT accepter_id AS id
-FROM RequestAccepted
-)
-SELECT id, COUNT(*) AS num
-FROM merged
+SELECT a.id, COUNT(*) AS num
+FROM (
+    SELECT requester_id AS id
+    FROM RequestAccepted 
+    UNION ALL
+    SELECT accepter_id  AS id
+    FROM RequestAccepted
+) a
 GROUP BY id
-ORDER BY num DESC
-LIMIT 1
-;
-
-
+ORDER BY num DESC 
+LIMIT 1;
