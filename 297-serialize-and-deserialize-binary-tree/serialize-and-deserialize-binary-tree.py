2e4c17a344ce192e72d6572f45b82892f86e8f6c
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2025-10-24 21:15:37 -0500

    Time: 102 ms (18.70%) | Memory: 23.9 MB (6.45%) - LeetSync

---
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
@@ -0,0 +1,15 @@
+class Codec:
+    def serialize(self, root):
+        if not root: return 'x'
+        return ','.join([str(root.val), self.serialize(root.left), self.serialize(root.right)])
+
+    def deserialize(self, data):
+		# The reason I use self.data in the deserialize is, data stream will be consumed  as we build left side of Tree
+		# by the time when the right side is building up, we need to hold what is left over.
+		# Therefore, self.data is a global value, right side will use what is left over after tree is partially built
+        self.data = data
+        if self.data[0] == 'x': return None
+        node = TreeNode(self.data[:self.data.find(',')]) 
+        node.left = self.deserialize(self.data[self.data.find(',')+1:])
+        node.right = self.deserialize(self.data[self.data.find(',')+1:])
+        return node
