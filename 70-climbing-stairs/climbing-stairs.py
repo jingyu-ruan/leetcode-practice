9321087cfee0089ee37502be5ef6b3566db1f486
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2025-11-12 20:05:41 -0600

    Time: 0 ms (100.00%) | Memory: 17.9 MB (30.05%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -4,11 +4,12 @@ class Solution:
             return 1
         if n == 2:
             return 2
-        dp = [0] * (n + 1)
-        dp[0] = 0
-        dp[1] = 1
-        dp[2] = 2
-        for i in range(3, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
+        dp = [0] * n
         
-        return dp[n]
+        dp[0] = 1
+        dp[1] = 2
+
+        for i in range(2, n):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[-1]
