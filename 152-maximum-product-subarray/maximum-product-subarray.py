a9fe430cccbc57cd41cdd462cd110569876f9515
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2025-11-13 12:02:29 -0600

    Time: 10 ms (58.19%) | Memory: 18.3 MB (80.95%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -5,11 +5,10 @@ class Solution:
         res = nums[0]
 
         for x in nums[1:]:
-            if x < 0:
-                cur_max, cur_min = cur_min, cur_max
+            #if x < 0:
+            #    cur_max, cur_min = cur_min, cur_max
 
-            cur_max = max(x, cur_max * x)
-            cur_min = min(x, cur_min * x)
+            cur_max, cur_min = max(x, cur_max * x, cur_min * x), min(x, cur_min * x, cur_max * x)
 
             res = max(res, cur_max)
         
