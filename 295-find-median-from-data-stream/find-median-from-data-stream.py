dda727862413ea1e4d1745f7f16d60e52092474b
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2025-11-17 20:13:19 -0600

    Time: 177 ms (65.03%) | Memory: 41.3 MB (6.11%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -1,14 +1,27 @@
-from sortedcontainers import SortedList
+import heapq
 class MedianFinder:
 
     def __init__(self):
-        self.arr = SortedList()
+        self.small = []
+        self.large = []
 
     def addNum(self, num: int) -> None:
-        self.arr.add(num)
+        heapq.heappush(self.small, -num)
+
+        if self.large and -self.small[0] > self.large[0]:
+            val = -heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+
+        if len(self.small) > len(self.large) + 1:
+            val = heapq.heappop(self.small)
+            heapq.heappush(self.large, -val)
+        
+        if len(self.large) > len(self.small):
+            val = heapq.heappop(self.large)
+            heapq.heappush(self.small, -val)
 
     def findMedian(self) -> float:
-        n = len(self.arr)
-        if n % 2 == 1:
-            return self.arr[n//2]
-        return (self.arr[n//2] + self.arr[n//2-1]) / 2
+        if len(self.small) > len(self.large):
+            return -self.small[0]
+        else:
+            return (-self.small[0] + self.large[0]) / 2
