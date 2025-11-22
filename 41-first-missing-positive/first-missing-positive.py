c0c13c32a85d8b4842371974fee28c6a08695d74
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2025-11-22 12:53:07 -0600

    Time: 41 ms (63.20%) | Memory: 28.9 MB (74.07%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -1,11 +1,14 @@
-import heapq
+
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        res = 1
-        heapq.heapify(nums)
-        while nums:
-            num = heapq.heappop(nums)
-            if num > 0 and res == num:
-                res += 1
+        n = len(nums)
 
-        return res
+        for i in range(n):
+            while 1 <= nums[i] <= n and nums[nums[i] - 1] != nums[i]:
+                correct = nums[i] - 1
+                nums[i], nums[correct] = nums[correct], nums[i]
+        for i in range(n):
+            if nums[i] != i + 1:
+                return i + 1
+        
+        return n + 1
