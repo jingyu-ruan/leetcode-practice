78f5d1f8a2caa0462c906ce79e5593b66ab20db0
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2025-11-23 10:32:14 -0600

    Time: 7 ms (58.49%) | Memory: 19.5 MB (19.27%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,8 +1,16 @@
 from collections import defaultdict
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        dic = defaultdict(int)
+        cnt = 0
+        candi = 0
+
         for i in nums:
-            dic[i] += 1
+            if cnt == 0:
+                candi = i
+            
+            if i == candi:
+                cnt += 1
+            else:
+                cnt -= 1
         
-        return max(dic, key=dic.get)
+        return candi
