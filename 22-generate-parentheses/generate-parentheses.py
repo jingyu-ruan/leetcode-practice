fcfa18f4f8695443d833623e023ef9e6cbcb7346
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2025-11-24 12:11:59 -0600

    Time: 3 ms (31.18%) | Memory: 17.9 MB (92.44%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,22 +1,20 @@
-from collections import deque
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        queue = deque(['('])
         res = []
-        while queue:
-            q_len = len(queue)
-            for _ in range(q_len):
-                str = queue.popleft()
-                if str.count('(') == str.count(')') and str.count('(') < n:
-                    queue.append(str + '(')
-                if str.count(')') == n:
-                    res.append(str)
-                if str.count('(') > str.count(')'):
-                    if str.count('(') < n:
-                        queue.append(str + '(')
-                        queue.append(str + ')')
-                    else:
-                        queue.append(str + ')')
+        def dfs(str):
+            if len(str) == 2 * n:
+                res.append(str)
+                return
+            
+            if str.count('(') == str.count(')'):
+                dfs(str + '(')
+            if str.count('(') > str.count(')'):
+                if str.count('(') < n:
+                    dfs(str + '(')
+                    dfs(str + ')')
+                else:
+                    dfs(str + ')')
+        
+        dfs('(')
 
         return res
-                    
