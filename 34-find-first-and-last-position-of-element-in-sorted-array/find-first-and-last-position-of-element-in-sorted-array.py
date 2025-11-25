a1d2bf46a2453f6b39973d601905181956e5f470
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2025-11-25 13:17:54 -0600

    Time: 0 ms (100.00%) | Memory: 19.2 MB (20.28%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,26 +1,32 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        if not nums:
-            return [-1, -1]
-        res = [-1, -1]
         l, r = 0, len(nums) - 1
-
-        while l <= r:
+        start, end = -1, -1
+        while l <= r: # 确定左界
             m = (l + r) // 2
-            if nums[m] == target:
-                res[0], res[1] = m, m
-                cur = m - 1
-                while cur >= 0 and nums[cur] == target:
-                    res[0] = cur
-                    cur -= 1
-                nxt = m + 1
-                while nxt <= len(nums) - 1 and nums[nxt] == target :
-                    res[1] = nxt
-                    nxt += 1
-                break
+            if nums[m] > target:
+                r = m - 1
             elif nums[m] < target:
                 l = m + 1
-            else:
+            else: # nums[m] == target
+                if m == 0 or nums[m-1] != target:
+                    start = m
+                    break
+                else:
+                    r = m - 1
+
+        l, r = 0, len(nums) - 1
+        while l <= r: # 确定右界
+            m = (l + r) // 2
+            if nums[m] > target:
                 r = m - 1
-            
-        return res
+            elif nums[m] < target:
+                l = m + 1
+            else: # nums[m] == target
+                if m == len(nums) - 1 or nums[m+1] != target:
+                    end = m
+                    break
+                else:
+                    l = m + 1
+
+        return [start, end]
