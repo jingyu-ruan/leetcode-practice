defc326808aa082d0a4d601c8c1d671d2f8d52e0
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2025-11-26 12:59:34 -0600

    Time: 3 ms (10.96%) | Memory: 17.9 MB (72.19%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.py
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.py
@@ -4,23 +4,24 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
+from collections import deque
 class Solution:
     def flatten(self, root: Optional[TreeNode]) -> None:
         """
         Do not return anything, modify root in-place instead.
         """
-        if not root:
-            return
-        nodes = []
-        def preorder(node):
-            if not node:
-                return
-            nodes.append(node)
-            preorder(node.left)
-            preorder(node.right)
-        
-        preorder(root)
-        for i in range(len(nodes) - 1):
-            nodes[i].left = None
-            nodes[i].right = nodes[i + 1]
+        if root:
+            nodes = deque()
+            def dfs(node):
+                if not node:
+                    return
+                nodes.append(node)
+                dfs(node.left)
+                dfs(node.right)
+            dfs(root)
+
+            for i in range(1, len(nodes)):
+                prev = nodes[i - 1]
+                cur = nodes[i]
+                prev.left = None
+                prev.right = cur
