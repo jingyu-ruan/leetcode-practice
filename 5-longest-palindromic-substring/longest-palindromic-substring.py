4c2ade301975305af1ef04d4e6fbdf90ae43db5d
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2025-11-28 10:57:43 -0600

    Time: 211 ms (92.93%) | Memory: 17.8 MB (59.55%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,43 +1,31 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if not s:
-            return ''
-        if len(s) == 1:
-            return s
-        if len(s) == 2:
-            if s[0] == s[1]:
-                return s
-            else:
-                return s[0]
-
-        max_len = 1
-        res = s[0]
-
-        for i in range(1, len(s) - 1):
-            ch = s[i]
-            l = i-1
-            r = i+1
-            cur_len = 1
-            while l >= 0 and r < len(s) and s[l] == s[r]:  # odd
-                cur_len += 2
-                l -= 1
-                r += 1
-            if cur_len > max_len:
-                max_len = cur_len
-                res = s[l+1:r]
-        
-        for i in range(0, len(s) - 1):
-            ch = s[i]
+        n = len(s)
+        max_len = 0
+        res = ''
+        for i in range(n):
             l = i
-            r = i+1
-            cur_len = 1
-            while l >= 0 and r < len(s) and s[l] == s[r]:  # even
-                cur_len += 2
-                l -= 1
-                r += 1
-            if cur_len > max_len:
-                max_len = cur_len
+            r = i
+            while l >= 0 and r < n:
+                if s[l] == s[r]:
+                    l -= 1
+                    r += 1
+                else:
+                    break
+            if (r - l - 1) > max_len:
+                max_len = r - l - 1
+                res = s[l+1:r]
+
+            l = i
+            r = i + 1
+            while l >= 0 and r < n:
+                if s[l] == s[r]:
+                    l -= 1
+                    r += 1
+                else:
+                    break
+            if r - l > 1 and (r - l - 1) > max_len:
+                max_len = r - l - 1
                 res = s[l+1:r]
 
         return res
-            
