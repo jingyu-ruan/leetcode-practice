d714baed83971716e11c02a849ca3929b6568cd4
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2025-11-28 12:32:58 -0600

    Time: 0 ms (100.00%) | Memory: 17.8 MB (37.45%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,12 +1,16 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        res = []
-
-        for i in range(numRows):
-            dp = [1] * (i+1)
-            if i >= 2:
-                for j in range(1, i):
-                    dp[j] = res[-1][j-1] + res[-1][j]
-            res.append(dp)
-
+        if numRows == 1:
+            return [[1]]
+        if numRows == 2:
+            return [[1], [1, 1]]
+        
+        res = [[1], [1, 1]]
+        for i in range(3, numRows + 1):
+            path = [1] * i
+            for j in range(1, i - 1):
+                path[j] = res[-1][j-1] + res[-1][j]
+            
+            res.append(path)
+        
         return res
