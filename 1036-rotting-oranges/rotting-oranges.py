d5493a708b549da155de0858b5eaeaf3a5361452
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2025-11-30 11:53:19 -0600

    Time: 7 ms (24.98%) | Memory: 17.8 MB (70.66%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -2,44 +2,38 @@ from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         flatten = []
-
-        for i in range(len(grid)):
-            flatten.extend(grid[i])
-        if 1 not in flatten:
+        for lst in grid:
+            flatten.extend(lst)
+        if 2 not in flatten and 1 not in flatten:
+            return 0
+        if 2 in flatten and 1 not in flatten:
             return 0
         if 2 not in flatten:
             return -1
-        
-        queue = deque()
         m = len(grid)
         n = len(grid[0])
-        res = 0
+        queue = deque()
 
-        for r in range(m):
-            for c in range(n):
-                if grid[r][c] == 2:
-                    queue.append((r, c))
-        
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 2:
+                    queue.append((i, j))
+        res = 0
         while queue:
-            cur_orgs = len(queue)
-            for o in range(cur_orgs):
-                cur = queue.popleft()
-                a, b = cur[0], cur[1]
-                if a - 1 >= 0 and grid[a-1][b] == 1:
-                    queue.append((a-1, b))
-                    grid[a-1][b] = 2
-                if a + 1 < m and grid[a+1][b] == 1:
-                    queue.append((a+1, b))
-                    grid[a+1][b] = 2
-                if b - 1 >= 0 and grid[a][b-1] == 1:
-                    queue.append((a, b-1))
-                    grid[a][b-1] = 2
-                if b + 1 < n and grid[a][b+1] == 1:
-                    queue.append((a, b+1))
-                    grid[a][b+1] = 2
+            cur_len = len(queue)
+            for _ in range(cur_len):
+                i, j = queue.popleft()
+                dir = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+                for a, b in dir:
+                    if 0 <= i + a < m and 0 <= j + b < n and grid[i+a][j+b] == 1:
+                        grid[i+a][j+b] = 2
+                        queue.append((i+a, j+b))
             res += 1
 
-        for f in range(m):
-            if 1 in grid[f]:
-                return -1
+        flatten = []
+        for lst in grid:
+            flatten.extend(lst)
+        if 1 in flatten:
+            return -1
+
         return res - 1
