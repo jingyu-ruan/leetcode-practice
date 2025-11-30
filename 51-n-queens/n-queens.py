ac332d9fe7ddba51a966d9b892b881d5366eb537
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2025-11-30 17:38:30 -0600

    Time: 11 ms (71.22%) | Memory: 18.6 MB (15.67%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,31 +1,38 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        board = [['.'] * n for _ in range(n)]
         cols = set()
         dia1 = set()
         dia2 = set()
         res = []
-        def dfs(r):
-            if r == n:
-                solution = [''.join(row) for row in board]
-                res.append(solution)
-                return 
+        path = []
+        queens = []
+        def dfs(i):
+            # base case
+            if i == n:
+                queens.append(path[:])
+                return
 
-            for c in range(n):
-                if c in cols or r + c in dia1 or r - c in dia2:
-                    continue
-
-                cols.add(c)
-                dia1.add(r + c)
-                dia2.add(r - c)
-                board[r][c] = 'Q'
-
-                dfs(r + 1)
-
-                cols.remove(c)
-                dia1.remove(r + c)
-                dia2.remove(r - c)
-                board[r][c] = '.'
-
+            for j in range(n):
+                if j not in cols and i + j not in dia1 and i - j not in dia2:
+                    cols.add(j)
+                    dia1.add(i + j)
+                    dia2.add(i - j)
+                    path.append((i, j))
+                    dfs(i + 1)
+                    cols.remove(j)
+                    dia1.remove(i + j)
+                    dia2.remove(i - j)
+                    path.pop()
+        
         dfs(0)
+
+        for pos in queens:
+            board = [['.'] * n for _ in range(n)]
+            for i, j in pos:
+                board[i][j] = 'Q'
+            upload = []
+            for row in board:
+                upload.append(''.join(row))
+            res.append(upload)
+
         return res
