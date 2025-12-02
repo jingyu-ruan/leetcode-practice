855c9c7cfbe0bf995566f1946bd90a7a1b5c8e4f
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2025-12-02 10:00:57 -0600

    Time: 22 ms (49.11%) | Memory: 17.8 MB (81.77%) - LeetSync

---
+++ 383-ransom-note/ransom-note.py
@@ -0,0 +1,10 @@
+from collections import Counter
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        r = Counter(ransomNote)
+        m = Counter(magazine)
+        for i in r:
+            if r[i] > m[i]:
+                return False
+        
+        return True
