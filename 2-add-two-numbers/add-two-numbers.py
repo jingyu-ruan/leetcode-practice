26db2398fd5cad6c2599e8183c0f607b0fdeefe9
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2025-12-03 12:13:45 -0600

    Time: 8 ms (19.63%) | Memory: 17.8 MB (84.53%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -3,22 +3,27 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+from collections import deque
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        cur1 = l1
+        cur2 = l2
+        lst1 = deque()
+        lst2 = deque()
+        while cur1:
+            lst1.appendleft(cur1.val)
+            cur1 = cur1.next
+        while cur2:
+            lst2.appendleft(cur2.val)
+            cur2 = cur2.next
+        
+        sum_num = sum_num = str(int(''.join(str(i) for i in lst1)) + int(''.join(str(i) for i in lst2)))
         dummy = ListNode(0)
         cur = dummy
-        carry = 0
-        while l1 or l2 or carry:
-            v1 = l1.val if l1 else 0
-            v2 = l2.val if l2 else 0
-
-            total = v1 + v2 + carry
-            carry = total // 10
-            cur.next = ListNode(total % 10)
+        for i in reversed(sum_num):
+            cur.next = ListNode(int(i))
             cur = cur.next
-
-            if l1: l1 = l1.next
-            if l2: l2 = l2.next
+        
+        cur.next = None
         
         return dummy.next
-
