76a4aef459266a5647763dd383455b24ef19296f
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2025-12-03 18:25:15 -0600

    Time: 46 ms (69.97%) | Memory: 20 MB (31.22%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -6,12 +6,10 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        slow = head
-        fast = head
+        fast = slow = head
         while fast and fast.next:
             fast = fast.next.next
             slow = slow.next
-
             if fast == slow:
                 return True
         
