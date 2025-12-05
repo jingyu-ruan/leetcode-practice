3536159036af27b6b705dfb32ee2197ad34c6768
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2025-12-05 11:23:29 -0600

    Time: 3 ms (31.84%) | Memory: 18.7 MB (16.82%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
@@ -3,34 +3,38 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head.next:
+            return head
+
         dummy = ListNode(0, head)
-        group_prev = dummy
-
+        cur = dummy
+        last_end = dummy
         while True:
-            # 1. 找到这一组的第 k 个节点
-            kth = group_prev
-            for _ in range(k):
-                kth = kth.next
-                if not kth:
-                    # 不足 k 个, 不再反转
+            # last_end = cur
+            cur = last_end
+            for i in range(k):
+                cur = cur.next
+                if not cur:
                     return dummy.next
+            
+            cur_end = cur
+            nxt_start = cur.next
+            cur_start = last_end.next
+            last_end.next = None
+            cur_end.next = None
 
-            group_next = kth.next  # 下一组的起点
-
-            # 2. 反转这一组 [group_prev.next, kth]
-            prev = group_next
-            cur = group_prev.next
-            while cur != group_next:
+            prev = None
+            cur = cur_start
+            while cur:
                 nxt = cur.next
                 cur.next = prev
                 prev = cur
                 cur = nxt
 
-            # 3. 接回到大链表
-            # 此时 prev 是这一组反转后的头, group_prev.next 是这一组反转后的尾
-            tail = group_prev.next
-            group_prev.next = kth
-            group_prev = tail
+            last_end.next = cur_end
+            cur_start.next = nxt_start
+            last_end = cur_start
+            
+        return dummy.next
