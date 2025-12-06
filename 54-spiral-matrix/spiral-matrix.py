d7792760147af06e8b4f724b90648c463f0a7a58
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2025-12-06 12:00:21 -0600

    Time: 0 ms (100.00%) | Memory: 18 MB (18.04%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,34 +1,40 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         m, n = len(matrix), len(matrix[0])
-        res = []
-        top, bottom = 0, m - 1
+        top, down = 0, m - 1
         left, right = 0, n - 1
-
-        while top <= bottom and left <= right:
-            # 从左到右
-            for c in range(left, right + 1):
-                res.append(matrix[top][c])
+        res = []
+        nums = 0
+        while top <= down and left <= right:
+            for i in range(left, right + 1):
+                res.append(matrix[top][i])
+                nums += 1
+            if nums == m * n:
+                break
             top += 1
 
-            # 从上到下
-            for r in range(top, bottom + 1):
-                res.append(matrix[r][right])
+            for i in range(top, down + 1):
+                res.append(matrix[i][right])
+                nums += 1
+            if nums == m * n:
+                break
+            
             right -= 1
 
-            # 检查边界是否交叉
-            if top > bottom or left > right:
+            for i in range(right, left - 1, -1):
+                res.append(matrix[down][i])
+                nums += 1
+            if nums == m * n:
                 break
 
-            # 从右到左
-            for c in range(right, left - 1, -1):
-                res.append(matrix[bottom][c])
-            bottom -= 1
+            down -= 1
 
-            # 从下到上
-            for r in range(bottom, top - 1, -1):
-                res.append(matrix[r][left])
+            for i in range(down, top - 1, -1):
+                res.append(matrix[i][left])
+                nums += 1
+            if nums == m * n:
+                break
+            
             left += 1
 
         return res
-    
