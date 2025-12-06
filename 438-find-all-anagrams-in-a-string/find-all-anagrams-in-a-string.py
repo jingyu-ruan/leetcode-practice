cb68ca9c2e3600bb75181ce93c065c51b595b6d4
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2025-12-06 15:03:21 -0600

    Time: 37 ms (74.18%) | Memory: 18.6 MB (16.25%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,28 +1,28 @@
 from collections import Counter, defaultdict
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        if len(s) < len(p):
-            return []
         need = Counter(p)
-        cur = defaultdict(int)
-        #need_len = len(need)
-        #cur_len = 0
-        # l = 0
+        window = defaultdict(int)
         res = []
-        # create window
-        for i in range(len(p)):
-            cur[s[i]] += 1
-        
-        if cur == need:
-            res.append(0)
-        
-        for r in range(len(p), len(s)):
-            cur[s[r]] += 1
-            cur[s[r-len(p)]] -= 1
-            if cur[s[r-len(p)]] == 0:
-                del cur[s[r-len(p)]]
-            if cur == need:
-                res.append(r - len(p) + 1)
+        l = 0
+        fulfilled = 0
+
+        for i, v in enumerate(s):
+            if v in need:
+                window[v] += 1
+                if window[v] == need[v]:
+                    fulfilled += 1
             
-        
-        return res
+            
+            if i - l + 1 > len(p):
+                d = s[l]
+                l += 1
+                if d in need:
+                    if window[d] == need[d]:
+                        fulfilled -= 1 
+                    window[d] -= 1
+                       
+
+            if fulfilled == len(need):
+                res.append(l)
+        return res            
