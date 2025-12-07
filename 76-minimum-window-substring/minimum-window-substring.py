2d6a8db4adae42ebf66a3f8e3a6a5f36f964289c
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2025-12-06 20:29:45 -0600

    Time: 131 ms (31.43%) | Memory: 18.6 MB (6.66%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,34 +1,35 @@
 from collections import Counter, defaultdict
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        n1, n2 = len(s), len(t)
-        if n2 > n1:
+        if len(t) > len(s):
             return ''
-        t_cnt = Counter(t)
-        fulfilled = 0
-        need = len(t_cnt)
-        l = 0
-        start = 0
-        min_len = float('inf')
-        window = defaultdict(int)
-        for i, v in enumerate(s):
-            if v in t_cnt:
-                window[v] += 1
-                if window[v] == t_cnt[v]:
-                    fulfilled += 1
-            
-            while fulfilled == need:
-                if i - l + 1 < min_len:
-                    start = l
-                    min_len = i - l + 1
-                
-                d = s[l]
-                l += 1
-
-                if d in t_cnt:
-                    if window[d] == t_cnt[d]:
-                        fulfilled -= 1
-                    window[d] -= 1
         
-        return "" if min_len == float('inf') else s[start : start + min_len]
+        need = Counter(t)
+        fulfilled = 0
+        l = 0
+        window = defaultdict(int)
+        min_len = float('inf')
+        res = ''
+        for i, v in enumerate(s):
+            if v in need:
+                window[v] += 1
+                if window[v] == need[v]:
+                    fulfilled += 1
+
+            while fulfilled == len(need):
+                d = s[l]
+                if d in need:
+                    window[d] -= 1
+                    if window[d] < need[d]:
+                        fulfilled -= 1
+                l += 1
+            
+                if i - l + 2 < min_len:
+                    min_len = i - l + 2
+                    res = s[l-1:i+1]
+            
+        
+        return res
+
+
 
