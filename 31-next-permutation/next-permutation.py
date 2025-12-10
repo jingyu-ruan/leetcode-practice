ebbc6d88faba7376e79a25ccac46d50f98b5eaff
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2025-12-10 10:54:58 -0600

    Time: 0 ms (100.00%) | Memory: 17.7 MB (86.07%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -5,18 +5,17 @@ class Solution:
         """
         n = len(nums)
         i = n - 2
-        while i >= 0 and nums[i] >=nums[i+1]:
+        while i >= 0 and nums[i] >= nums[i + 1]:
             i -= 1
         
         if i >= 0:
             j = n - 1
-            while nums[j] <= nums[i]:
+            while j >= 0 and nums[j] <= nums[i]:
                 j -= 1
-            
             nums[i], nums[j] = nums[j], nums[i]
         
-        l, r = i + 1, n - 1
-        while l < r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l += 1
-            r -= 1
+        left, right = i + 1, n - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
