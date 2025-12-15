63e40ca65bd12ef1e6fb13aa23c222f2d53668e7
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2025-12-15 12:14:53 -0600

    Time: 699 ms (96.21%) | Memory: 52.6 MB (85.52%) - LeetSync

--- 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
@@ -1,38 +1,37 @@
 class WordDictionary:
-    class Node:
-        __slots__ = ("children", "is_end")
-        def __init__(self):
-            self.children = {}      # key: 字母, value: Node
-            self.is_end = False     # 该节点是否是某个单词的结尾
-
     def __init__(self):
-        self.root = WordDictionary.Node()
+        self.dic = dict()
 
     def addWord(self, word: str) -> None:
-        cur = self.root
-        for ch in word:
-            if ch not in cur.children:
-                cur.children[ch] = WordDictionary.Node()
-            cur = cur.children[ch]
-        cur.is_end = True
+        cur = self.dic
+        for i in word:
+            if i not in cur:
+                cur[i] = dict()
+            cur = cur[i]
+        cur['#'] = True
 
     def search(self, word: str) -> bool:
-        # 在以 node 为根的子树里，从索引 i 开始匹配 word[i:]
-        def dfs(i: int, node: WordDictionary.Node) -> bool:
+        def dfs(cur, i):
             if i == len(word):
-                return node.is_end
-
-            ch = word[i]
-            if ch == ".":
-                # 通配符: 尝试所有子节点
-                for nxt in node.children.values():
-                    if dfs(i + 1, nxt):
-                        return True
-                return False
-            else:
-                # 普通字母: 必须沿着固定分支走
-                if ch not in node.children:
+                return '#' in cur
+            
+            if word[i] != '.':
+                if word[i] not in cur:
                     return False
-                return dfs(i + 1, node.children[ch])
+                return dfs(cur[word[i]], i + 1)
+            
+            for nxt in cur:
+                if nxt != '#':
+                    if dfs(cur[nxt], i + 1):
+                        return True
+            
+            return False
+        
+        return dfs(self.dic, 0)
 
-        return dfs(0, self.root)
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
