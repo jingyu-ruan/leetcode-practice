9ffca8135de4ca66229f41c0f4d580b4db629b38
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2025-12-15 11:18:40 -0600

    Time: 41 ms (73.35%) | Memory: 30.6 MB (85.69%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
@@ -1,30 +1,34 @@
 class Trie:
-
     def __init__(self):
-        self.root = {}
+        self.dic = dict()
+        self.cur = self.dic
 
     def insert(self, word: str) -> None:
-        node = self.root
-        for ch in word:
-            if ch not in node:
-                node[ch] = {}
-            node = node[ch]
-        node['#'] = True
+        self.cur = self.dic
+        for i in word:
+            if i not in self.cur:
+                self.cur[i] = dict()
+            self.cur = self.cur[i]
+        self.cur['#'] = True
 
     def search(self, word: str) -> bool:
-        node = self.root
-        for ch in word:
-            if ch not in node:
+        self.cur = self.dic
+        for i in word:
+            if i not in self.cur:
                 return False
-            node = node[ch]
-        return '#' in node
+            self.cur = self.cur[i]
+        if '#' in self.cur:
+            return True
+        
+        return False
 
     def startsWith(self, prefix: str) -> bool:
-        node = self.root
-        for ch in prefix:
-            if ch not in node:
+        self.cur = self.dic
+        for i in prefix:
+            if i not in self.cur:
                 return False
-            node = node[ch]
+            self.cur = self.cur[i]
+        
         return True
 
 
