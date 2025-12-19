cdd1e0ca7c6723418edfc949179649f2eb104825
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2025-12-19 12:14:49 -0600

    Time: 3 ms (33.06%) | Memory: 17.5 MB (99.99%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,16 +1,15 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        res = []
         n = len(nums)
-        def dfs(lst, remains):
-            if len(lst) == n:
-                    res.append(lst)
-                    return
-
-            for i in remains:
-                new_remains = [x for x in remains if x != i]
-                new_lst = lst + [i]
-                dfs(new_lst, new_remains)
+        res = []
+        def dfs(path, num_set, i):
+            if i == n:
+                res.append(path[:])
+            
+            for j in range(n):
+                if nums[j] not in num_set:
+                    dfs(path + [nums[j]], num_set.union({nums[j]}), i + 1)
         
-        dfs([], nums)
+        dfs([], set(), 0)
+
         return res
