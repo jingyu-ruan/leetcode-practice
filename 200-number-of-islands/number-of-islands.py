de726fb9744559e7361e30aa64295b04ef24f179
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2026-01-31 13:37:47 -0600

    Time: 250 ms (46.24%) | Memory: 21.7 MB (52.34%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,23 +1,23 @@
+from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m, n = len(grid), len(grid[0])
         res = 0
-
-        def dfs(i, j):
-            if not 0 <= i < m or not 0 <= j < n:
-                return
-            if grid[i][j] != '1':
-                return
-            grid[i][j] = '#'
-            dir = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-            for a, b in dir:
-                dfs(i + a, j + b)
-        
+        q = deque()
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == '1':
+                    q.append((i, j))
                     res += 1
-                    dfs(i, j)
-        
+                    grid[i][j] = '#'
+                    while q:
+                        r, c = q.popleft()
+                        dir = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+                        for a, b in dir:
+                            if 0 <= r + a < m and 0 <= c + b < n and grid[r+a][c+b] == '1':
+                                q.append((r + a, c + b))
+                                grid[r + a][c + b] = '#'
         return res
+
+
         
