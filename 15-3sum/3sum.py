6fe8e1b09dd25f21033dbe0997796b08986a5010
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2026-01-31 13:20:37 -0600

    Time: 471 ms (86.14%) | Memory: 22.4 MB (23.78%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -6,22 +6,22 @@ class Solution:
         for i in range(n - 2):
             if nums[i] > 0:
                 break
-            if i > 0 and nums[i] == nums[i - 1]:
+            if (i > 0 and nums[i] == nums[i-1]):
                 continue
-            l, r = i + 1, n - 1
+            l = i + 1
+            r = n - 1
             while l < r:
-                s = nums[i] + nums[l] + nums[r]
-                if s == 0:
+                total = nums[i] + nums[l] + nums[r]
+                if total > 0:
+                    r -= 1
+                elif total < 0:
+                    l += 1
+                else:
                     res.append([nums[i], nums[l], nums[r]])
-                    while l + 1 < r and nums[l + 1] == nums[l]:
+                    l += 1
+                    r -= 1
+                    while l < n and nums[l] == nums[l-1]:
                         l += 1
-                    while r - 1 > l and nums[r - 1] == nums[r]:
+                    while r > i and nums[r] == nums[r+1]:
                         r -= 1
-                    l += 1
-                    r -= 1
-                elif s < 0:
-                    l += 1
-                elif s > 0:
-                    r -= 1
-        
         return res
