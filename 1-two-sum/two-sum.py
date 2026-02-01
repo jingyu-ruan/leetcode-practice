beb1f21ef96420ae803c2d11c115ebf8cafd18c8
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2026-02-01 12:12:39 -0600

    Time: 0 ms (100.00%) | Memory: 20.8 MB (9.81%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,8 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        matc = {}
-        for i, num in enumerate(nums):
-            need = target - num
-            if need in matc:
-                return [matc[need], i]
-            matc[num] = i
+        cache = dict()
+        for i, v in enumerate(nums):
+            if v in cache:
+                return [cache[v], i]
+            cache[target - v] = i
