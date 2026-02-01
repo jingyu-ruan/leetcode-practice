0199aecadf06ddbc8792b4343ebdc075b1943e9b
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2026-02-01 11:29:36 -0600

    Time: 0 ms (100.00%) | Memory: 19.2 MB (43.51%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,40 +1,38 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         m, n = len(matrix), len(matrix[0])
-        top, down = 0, m - 1
+        up, down = 0, m - 1
         left, right = 0, n - 1
         res = []
-        nums = 0
-        while top <= down and left <= right:
+        cur = 0
+        while True:
             for i in range(left, right + 1):
-                res.append(matrix[top][i])
-                nums += 1
-            if nums == m * n:
-                break
-            top += 1
+                res.append(matrix[up][i])
+                cur += 1
+            up += 1
+            if cur == m * n:
+                return res
 
-            for i in range(top, down + 1):
+            for i in range(up, down + 1):
                 res.append(matrix[i][right])
-                nums += 1
-            if nums == m * n:
-                break
-            
+                cur += 1
             right -= 1
+            if cur == m * n:
+                return res
 
             for i in range(right, left - 1, -1):
                 res.append(matrix[down][i])
-                nums += 1
-            if nums == m * n:
-                break
-
+                cur += 1
             down -= 1
-
-            for i in range(down, top - 1, -1):
-                res.append(matrix[i][left])
-                nums += 1
-            if nums == m * n:
-                break
+           
+            if cur == m * n:
+                return res
             
+            for i in range(down, up - 1, -1):
+                res.append(matrix[i][left])
+                cur += 1
             left += 1
-
+            if cur == m * n:
+                return res
+            
         return res
