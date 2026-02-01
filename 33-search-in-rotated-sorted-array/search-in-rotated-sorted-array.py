e9afa50e4419234b13107605132b6ffb28ec1b6d
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2026-02-01 11:18:10 -0600

    Time: 0 ms (100.00%) | Memory: 19.5 MB (18.77%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -3,17 +3,17 @@ class Solution:
         l, r = 0, len(nums) - 1
         while l <= r:
             m = (l + r) // 2
-
-            if nums[l] <= nums[m]: # 左边单调递增
+            if nums[m] == target:
+                return m
+            if nums[l] <= nums[m]:
                 if nums[l] <= target < nums[m]:
                     r = m - 1
                 else:
                     l = m + 1
-            else:  # 右边单调递增
+            else: # [6, 7, 0, 1, 2, 4, 5]
                 if nums[m] < target <= nums[r]:
                     l = m + 1
                 else:
-                    r = m - 1            
-            if nums[m] == target:
-                return m    
+                    r = m - 1
+
         return -1
