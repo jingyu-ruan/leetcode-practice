3e3a9389056a4061f84b55d996398ebf93ba10b2
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2026-02-01 11:55:05 -0600

    Time: 10 ms (62.57%) | Memory: 21.1 MB (37.66%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -3,18 +3,17 @@ class Solution:
         n = len(height)
         left = [0] * n
         right = [0] * n
-
-        left_max = height[0]
+        pre = height[0]
         for i in range(1, n):
-            left[i] = left_max
-            left_max = max(left_max, height[i])
-        right_max = height[-1]
+            left[i] = pre
+            pre = max(pre, height[i])
+        post = height[-1]
         for i in range(n - 2, -1, -1):
-            right[i] = right_max
-            right_max = max(right_max, height[i])
+            right[i] = post
+            post = max(post, height[i])
         
         res = 0
-        for i in range(n):
+        for i in range(1, n - 1):
             res += max(0, min(left[i], right[i]) - height[i])
         
         return res
