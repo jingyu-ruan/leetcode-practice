d9a48dd5e10f77e95ce16ffcf229c449d36b14c7
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2026-02-02 17:08:08 -0600

    Time: 3 ms (50.99%) | Memory: 27.9 MB (22.05%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -5,37 +5,30 @@
 #         self.next = next
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-        if not head or not head.next:
-            return 
-
-        slow, fast = head, head
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        fast = slow = head
         while fast and fast.next:
-            slow = slow.next
             fast = fast.next.next
-
-        l2 = slow.next
-        slow.next = None
-
-        prev = None
-        curr = l2
-
-        while curr:
-            nxt_temp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = nxt_temp
-
-        l2 = prev
-        l1 = head
-        while l2:
-            l2_nxt = l2.next
-            l1_nxt = l1.next
-
-            l1.next = l2
-            l2.next = l1_nxt
-            
-            l1 = l1_nxt
-            l2 = l2_nxt
+            slow = slow.next
         
+        second = slow.next 
+        slow.next = None
+        prev = None
+        cur = second
+        while cur:
+            nxt = cur.next
+            cur.next = prev
+            prev = cur
+            cur = nxt
+        
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
 
+            first.next = second
+            second.next = tmp1
+            
+            first, second = tmp1, tmp2
         
