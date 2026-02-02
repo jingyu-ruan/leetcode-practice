ef1794c14ccb299a3acbb9afa42e0325a16612b3
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2026-02-02 16:42:50 -0600

    Time: 0 ms (100.00%) | Memory: 19.5 MB (20.62%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -1,25 +1,29 @@
-
 class Solution:
-    def merge(self, nums1, m, nums2, n):
-        i = m - 1        # nums1 的有效部分末尾
-        j = n - 1        # nums2 的末尾
-        k = m + n - 1    # nums1 的总末尾
-
-        # 从后往前填
-        while i >= 0 and j >= 0:
-            if nums1[i] >= nums2[j]:
-                nums1[k] = nums1[i]
-                i -= 1
-            else:
-                nums1[k] = nums2[j]
-                j -= 1
-            k -= 1
-
-        # nums2 可能还没放完（nums1 没必要处理，因为剩下来的已经在前面）
-        while j >= 0:
-            nums1[k] = nums2[j]
-            j -= 1
-            k -= 1
-
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        l = m - 1
+        p = m + n - 1
+        s2 = n - 1
         
+        while l >= 0 and s2 >= 0:
+            if nums1[l] >= nums2[s2]:
+                nums1[p] = nums1[l]
+                l -= 1
+                p -= 1
+            else:
+                nums1[p] = nums2[s2]
+                s2 -= 1
+                p -= 1
+        
+        while l >= 0:
+            nums1[p] = nums1[l]
+            l -= 1
+            p -= 1
+        
+        while s2 >= 0:
+            nums1[p] = nums2[s2]
+            s2 -= 1
+            p -= 1
         
