5f757962188a96279934f11323ae7db1c24bb61c
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2026-02-03 10:36:31 -0600

    Time: 1 ms (43.50%) | Memory: 19.5 MB (38.49%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -5,12 +5,13 @@ class Solution:
             if left == right == n:
                 res.append(''.join(path[:]))
                 return
-                
-            if left == n or left > right:
+            if left == n:
                 dfs(left, right + 1, path + [')'])
+            
             if left < n:
+                if left > right:
+                    dfs(left, right + 1, path + [')'])
                 dfs(left + 1, right, path + ['('])
         
         dfs(0, 0, [])
         return res
-                
