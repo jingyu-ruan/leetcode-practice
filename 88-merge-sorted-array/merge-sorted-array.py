279aa6d8f0890ca5b795be8d29d723223e2199b9
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2026-02-03 10:13:15 -0600

    Time: 0 ms (100.00%) | Memory: 19.4 MB (20.62%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -2,28 +2,28 @@ class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
+        nums1: 1, 2, 3, 0, 0, 0
+                     p1       cur
+        nums2: 2, 5, 6
+                     p2
         """
-        l = m - 1
-        p = m + n - 1
-        s2 = n - 1
-        
-        while l >= 0 and s2 >= 0:
-            if nums1[l] >= nums2[s2]:
-                nums1[p] = nums1[l]
-                l -= 1
-                p -= 1
+        p1 = m - 1
+        p2 = n - 1
+        cur = m + n - 1
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] >= nums2[p2]:
+                nums1[cur] = nums1[p1]
+                p1 -= 1
+                cur -= 1
             else:
-                nums1[p] = nums2[s2]
-                s2 -= 1
-                p -= 1
-        
-        while l >= 0:
-            nums1[p] = nums1[l]
-            l -= 1
-            p -= 1
-        
-        while s2 >= 0:
-            nums1[p] = nums2[s2]
-            s2 -= 1
-            p -= 1
-        
+                nums1[cur] = nums2[p2]
+                p2 -= 1
+                cur -= 1
+        while p1 >= 0:
+            nums1[cur] = nums1[p1]
+            p1 -= 1
+            cur -= 1
+        while p2 >= 0:
+            nums1[cur] = nums2[p2]
+            p2 -= 1
+            cur -= 1
