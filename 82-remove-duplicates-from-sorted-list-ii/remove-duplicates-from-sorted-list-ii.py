a32b29fea829558f7af386bb2c3a7d653aa15b08
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2026-02-04 22:55:57 -0600

    Time: 3 ms (29.34%) | Memory: 19.6 MB (19.70%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.py
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.py
@@ -3,24 +3,27 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+from collections import defaultdict
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        '''
+        D 1 2 3 3 4 4 5
+            ↑ p r r
+        '''
         if not head:
-            return None
-        if not head.next:
-            return head
+            return
         dummy = ListNode(0, head)
         cur = head
-        left = dummy
-    
+        vals = defaultdict(int)
         while cur:
-            if cur.next and cur.val == cur.next.val:
-                duplicate_val = cur.val
-                while cur and cur.val == duplicate_val:
-                    cur = cur.next
-                left.next = cur
-            else:
-                left = cur
-                cur = cur.next
-        
+            vals[cur.val] += 1
+            cur = cur.next
+        cur = dummy
+        while cur:
+            nxt = cur.next
+            while nxt and vals[nxt.val] > 1:
+                nxt = nxt.next
+            cur.next = nxt
+            cur = cur.next
         return dummy.next
+            
