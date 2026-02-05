8f70bc8308f60a6e3077669f668d804943434c1f
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2026-02-05 14:27:28 -0600

    Time: 10 ms (63.07%) | Memory: 20.7 MB (10.54%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.py
+++ 32-longest-valid-parentheses/longest-valid-parentheses.py
@@ -8,11 +8,8 @@ class Solution:
             if ch == ')':
                 stack.pop()
                 if not stack:
-                    # 如果栈空了，说明刚才弹出的不是左括号，而是之前的“参照物”
-                    # 这意味着当前的 ')' 多余了，它变成了新的“参照物”
                     stack.append(i)
                 else:
-                    # 如果栈不空，说明匹配成功
-                    # 长度 = 当前下标 - 栈顶(即上一个未匹配的位置)
                     res = max(res, i - stack[-1])
+        
         return res
