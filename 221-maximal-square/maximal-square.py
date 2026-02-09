ab9783143aa026e4683f7033420064024ba6946a
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2026-02-09 16:19:52 -0600

    Time: 94 ms (66.79%) | Memory: 33.4 MB (36.71%) - LeetSync

--- 221-maximal-square/maximal-square.py
+++ 221-maximal-square/maximal-square.py
@@ -7,8 +7,7 @@ class Solution:
             for j in range(1, n + 1):
                 if matrix[i-1][j-1] == '1':
                     dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
-                    res = max(res, dp[i][j])
+                    res = max(res, dp[i][j] * dp[i][j])
         
-        return res * res
-                
-                
+        return res
+        
