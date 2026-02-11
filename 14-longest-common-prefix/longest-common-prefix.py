f041c5561a3af7e0bf0af646376a0859eed5bc33
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2026-02-10 20:49:21 -0600

    Time: 0 ms (100.00%) | Memory: 19.4 MB (21.12%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -4,14 +4,16 @@ class Solution:
         if n == 1:
             return strs[0]
         
-        first = list(strs[0])
+        i = 0
+        res = ''
+        while i < len(strs[0]):
+            for j in range(1, n):
+                s = strs[j]
+                if i >= len(s):
+                    return res
+                if s[i] != strs[0][i]:
+                    return res
+            res += strs[0][i]
+            i += 1
         
-        for i in range(1, n):
-            r = 0
-            for j, ch in enumerate(strs[i]):
-                if j < len(first) and ch == first[j]:
-                    r += 1
-                else: break
-            first = first[:r]
-        
-        return ''.join(first)
+        return strs[0]
