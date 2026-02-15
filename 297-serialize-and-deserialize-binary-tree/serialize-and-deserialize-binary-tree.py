b639b434c99f213b8588a0d6c16d02f810ee590b
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2026-02-15 12:56:25 -0600

    Time: 81 ms (83.28%) | Memory: 22.7 MB (42.24%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
@@ -1,15 +1,68 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+from collections import deque
 class Codec:
     def serialize(self, root):
-        if not root: return 'x'
-        return ','.join([str(root.val), self.serialize(root.left), self.serialize(root.right)])
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        """Encodes a tree to a single string."""
+        if not root:
+            return ''
+        res = []
+        q = deque([root])
+        while q:
+            node = q.popleft()
+            if node:
+                res.append(str(node.val))
+                # 只有非空节点才把孩子入队
+                q.append(node.left)
+                q.append(node.right)
+            else:
+                # 关键修正：遇到空节点，必须记录 "None"
+                res.append("None")
+        return ','.join(res)
 
     def deserialize(self, data):
-		# The reason I use self.data in the deserialize is, data stream will be consumed  as we build left side of Tree
-		# by the time when the right side is building up, we need to hold what is left over.
-		# Therefore, self.data is a global value, right side will use what is left over after tree is partially built
-        self.data = data
-        if self.data[0] == 'x': return None
-        node = TreeNode(self.data[:self.data.find(',')]) 
-        node.left = self.deserialize(self.data[self.data.find(',')+1:])
-        node.right = self.deserialize(self.data[self.data.find(',')+1:])
-        return node
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        if not data:
+            return None
+        
+        vals = data.split(',')
+        root = TreeNode(int(vals[0])) # 先建立根节点
+        q = deque([root])             # 队列只存"存活"的父节点
+        
+        i = 1 # 指针，指向 vals 中下一个要处理的值
+        
+        while q:
+            node = q.popleft() # 取出一个父节点
+            
+            # 1. 尝试找左孩子
+            if vals[i] != "None":
+                node.left = TreeNode(int(vals[i]))
+                q.append(node.left) # 只有非空节点才有资格入队当爹
+            i += 1
+            
+            # 2. 尝试找右孩子
+            if vals[i] != "None":
+                node.right = TreeNode(int(vals[i]))
+                q.append(node.right)
+            i += 1
+            
+        return root
+
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
