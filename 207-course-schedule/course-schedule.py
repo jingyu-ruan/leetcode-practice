1f9a38b3119b83627b93492c8e654854b646b9af
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2026-02-15 11:47:23 -0600

    Time: 1 ms (91.50%) | Memory: 20.4 MB (57.52%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,24 +1,23 @@
-from collections import defaultdict, deque
+from collections import deque, defaultdict
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        unlock = defaultdict(list)
         need = defaultdict(int)
+        unlock = defaultdict(list)
         for a, b in prerequisites:
             unlock[b].append(a)
             need[a] += 1
         q = deque()
-        finish = 0
-        for c in range(numCourses):
-            if c not in need:
-                q.append(c)
-                # finish += 1
+        finished = 0
+        for i in range(numCourses):
+            if i not in need:
+                q.append(i)
         while q:
             course = q.popleft()
-            finish += 1
-            if course in unlock:
-                for i in unlock[course]:
-                    need[i] -= 1
-                    if need[i] == 0:
-                        q.append(i)
-        return True if finish == numCourses else False
+            finished += 1
+            for i in unlock[course]:
+                need[i] -= 1
+                if need[i] == 0:
+                    q.append(i)
+
+        return True if finished == numCourses else False
         
