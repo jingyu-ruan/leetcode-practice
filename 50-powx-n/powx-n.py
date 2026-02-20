c53a98ff7a7d99fb68698b8ba3d808069c7efe1b
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2026-02-20 12:43:31 -0600

    Time: 0 ms (100.00%) | Memory: 19.5 MB (43.54%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,19 +1,18 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         if n == 0:
-            return 1.0
-
+            return 1
         if n < 0:
             x = 1 / x
-            n = -n
-
-        res = 1.0
+            n *= -1
+        
+        res = 1
         while n > 0:
-            if n % 2 == 1:      # 当前位是 1
+            if n % 2 == 1:
                 res *= x
-            x *= x              # x = x^2
-            n //= 2             # 指数右移一位
-
+                n -= 1
+            
+            x *= x
+            n /= 2
         return res
-
         
