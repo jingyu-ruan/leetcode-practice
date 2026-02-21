c85c4bd9e5383f8bc2add7570504c4ebefa47503
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2026-02-21 12:35:17 -0600

    Time: 11 ms (34.83%) | Memory: 19.8 MB (47.50%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,33 +1,16 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        p = []
+        pure = ''
         for ch in s:
-            if ch.isalpha() or ch.isdigit():
-                p.append(ch.lower())
+            if ch.isalpha():
+                pure += ch.lower()
+            if ch.isdigit():
+                pure += ch
+        l, r = 0, len(pure) - 1
+        while l < r:
+            if pure[l] != pure[r]:
+                return False
+            l += 1
+            r -= 1
         
-        if not p:
-            return True
-        if len(p) == 1:
-            return True
-
-        n = len(p)
-        m = n // 2
-        if n % 2 == 1:
-            r = m + 1
-            l = m - 1
-            while l >= 0 and r <= n - 1:
-                if p[l] != p[r]:
-                    return False
-                r += 1
-                l -= 1
-            return True
-        else:
-            r = m
-            l = m - 1
-            while l >= 0 and r <= n - 1:
-                if p[l] != p[r]:
-                    return False
-                r += 1
-                l -= 1
-            return True
-
+        return True
