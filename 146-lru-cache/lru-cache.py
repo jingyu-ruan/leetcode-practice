a4de2e82709592559aa1447e0e7b2d6315ebc169
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2026-02-28 13:01:20 -0600

    Time: 131 ms (48.21%) | Memory: 76.8 MB (84.22%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,54 +1,57 @@
 class Node:
-    def __init__(self, key=0, val=0):
-        self.key = key
+    def __init__(self, val=0, key=None, prev=None, next=None):
         self.val = val
-        self.prev = None
-        self.next = None
+        self.key = key
+        self.prev = prev
+        self.next = next
 
 class LRUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.cache = dict()
-        self.head = Node()
-        self.tail = Node()
+
+        self.head = Node(0)
+        self.tail = Node(0)
         self.head.next = self.tail
         self.tail.prev = self.head
 
-    def remove(self, node):
+    def _remove(self, node):
         left = node.prev
         right = node.next
         left.next = right
         right.prev = left
-
-    def add_to_head(self, node):
-        nxt = self.head.next
+    
+    def _append_to_head(self, node):
+        right = self.head.next
         self.head.next = node
         node.prev = self.head
-        node.next = nxt
-        nxt.prev = node
+        node.next = right
+        right.prev = node
 
     def get(self, key: int) -> int:
         if key not in self.cache:
             return -1
         node = self.cache[key]
-        self.remove(node)
-        self.add_to_head(node)
+        self._remove(node)
+        self._append_to_head(node)
         return node.val
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             node = self.cache[key]
             node.val = value
-            self.remove(node)
-            self.add_to_head(node)
+            self._remove(node)
+            self._append_to_head(node)
         else:
-            node = Node(key, value)
-            self.add_to_head(node)
+            node = Node(value, key)
             self.cache[key] = node
+            self._append_to_head(node)
             if len(self.cache) > self.capacity:
                 node = self.tail.prev
-                self.remove(node)
+                self._remove(node)
                 del self.cache[node.key]
+
+
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
