415096b4fa3d635078684879510b8e481f56b3fa
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2026-02-28 13:20:47 -0600

    Time: 7 ms (96.76%) | Memory: 19.3 MB (82.89%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,15 +1,22 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if not s:
-            return 0
-        word = dict()
+        '''
+           l
+        -1 0 1 2 3
+        #  a b c a
+        t m m z u x t
+        0 1 2 3 4 5 6 
+        '''
+        hist = dict()
         l = 0
         res = 0
-        for i in range(len(s)):
-            if s[i] not in word:
-                word[s[i]] = i
+        for i, ch in enumerate(s):
+            if ch not in hist:
+                hist[ch] = i
+                res = max(res, i - l + 1)
             else:
-                l = max(word[s[i]] + 1, l)
-                word[s[i]] = i
-            res = max(res, i - l + 1)
+                l = max(hist[ch] + 1, l)
+                hist[ch] = i
+                res = max(res, i - l + 1)
+        
         return res
