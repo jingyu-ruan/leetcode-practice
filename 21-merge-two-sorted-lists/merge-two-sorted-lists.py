6c44c71dfb7b99af044fd2a523eb800e1de263f6
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2026-02-28 13:47:54 -0600

    Time: 4 ms (4.40%) | Memory: 19.2 MB (89.35%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -5,30 +5,25 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if not list1:
-            return list2
-        if not list2:
-            return list1
-        p1, p2 = list1, list2
-        dummy = ListNode(0)
-        cur = dummy
-        while p1 and p2:
-            if p1.val <= p2.val:
-                cur.next = p1
-                p1 = p1.next
+        cur1 = list1
+        cur2 = list2
+        dummy = ListNode()
+        p = dummy
+        while cur1 and cur2:
+            if cur1.val <= cur2.val:
+                p.next = cur1
+                cur1 = cur1.next
             else:
-                cur.next = p2
-                p2 = p2.next
-            cur = cur.next
-        while p1:
-            cur.next = p1
-            p1 = p1.next
-            cur = cur.next
-        while p2:
-            cur.next = p2
-            p2 = p2.next
-            cur = cur.next
-        # cur.next = None
+                p.next = cur2
+                cur2 = cur2.next
+            p = p.next
+        while cur1:
+            p.next = cur1
+            cur1 = cur1.next
+            p = p.next
+        while cur2:
+            p.next = cur2
+            cur2 = cur2.next
+            p = p.next
+        p.next = None
         return dummy.next
-        
-
