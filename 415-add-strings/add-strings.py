ec351c8dc4fbd146fb84b2d676a245e1adaadf5f
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2026-03-01 11:36:33 -0600

    Time: 3 ms (87.70%) | Memory: 19.4 MB (67.03%) - LeetSync

--- 415-add-strings/add-strings.py
+++ 415-add-strings/add-strings.py
@@ -1,23 +1,26 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
+        '''
+        1 2 3 
+          1 1
+        
+        3 2 1
+        1 1
+        '''
+        rev1 = num1[::-1]
+        rev2 = num2[::-1]
         n1, n2 = len(num1), len(num2)
-        res = ['0'] * (max(n1, n2) + 1)
-        p1, p2 = n1 - 1, n2 - 1
+        # p = 0
         carry = 0
-        for i in range(len(res) - 1, -1, -1):
+        res = ''
+        for i in range(max(n1, n2)):
             total = carry
-            if p1 >= 0:
-                total += int(num1[p1])
-            if p2 >= 0:
-                total += int(num2[p2])
+            if i < n1:
+                total += int(rev1[i])
+            if i < n2:
+                total += int(rev2[i])
+            res += str(total % 10)
             carry = total // 10
-            res[i] = str(total % 10)
-
-            p1 -= 1
-            p2 -= 1
-        start = 0
-        while start < len(res) and res[start] == '0':
-            start += 1
-        if start == len(res):
-            return '0'
-        return ''.join(res[start:])
+        if carry > 0:
+            res += str(carry)
+        return res[::-1]
