84f1bc9239adb168823b7e4abbd4ca6d5124798a
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2026-03-02 15:36:17 -0600

    Time: 144 ms (71.74%) | Memory: 21.4 MB (93.61%) - LeetSync

--- 903-implement-rand10-using-rand7/implement-rand10-using-rand7.py
+++ 903-implement-rand10-using-rand7/implement-rand10-using-rand7.py
@@ -8,12 +8,6 @@ class Solution:
         :rtype: int
         """
         while True:
-            # 1. 构造 1-49 的均匀分布
-            # (row - 1) * 7 + col
-            # 范围: (0~6) * 7 + (1~7) = 1 ~ 49
             num = (rand7() - 1) * 7 + rand7()
-            
-            # 2. 拒绝采样：只接受 1-40
             if num <= 40:
-                # 3. 映射回 1-10
                 return (num - 1) % 10 + 1
