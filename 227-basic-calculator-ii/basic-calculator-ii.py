c1bb41744d5e40c9de954718d77343b0d76861ad
Jay Ruan <150403642+jingyu-ruan@users.noreply.github.com>
2026-03-09 14:47:43 -0500

    Time: 71 ms (27.65%) | Memory: 22.4 MB (58.33%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -5,20 +5,19 @@ class Solution:
         stack = []
         num = 0
         sign = '+'
+
         for i, ch in enumerate(s):
             if ch.isdigit():
                 num = num * 10 + int(ch)
-            
-            if (not ch.isdigit() and ch != ' ') or i == len(s) - 1:
+            if ch in ('+', '-', '*', '/') or i == len(s) - 1:
                 if sign == '+':
                     stack.append(num)
-                elif sign == '-':
+                if sign == '-':
                     stack.append(-num)
-                elif sign == '*':
+                if sign == '*':
                     stack.append(stack.pop() * num)
-                elif sign == '/':
-                    a = stack.pop()
-                    stack.append(int(a / num))
+                if sign == '/':
+                    stack.append(int(stack.pop() / num))
                 
                 num = 0
                 sign = ch
